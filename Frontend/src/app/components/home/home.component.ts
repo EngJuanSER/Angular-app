32bda64cd8b4e454e73c6f0f7bc60a899e5c9916
Juan Manuel Serrano Rodriguez <jumserranor@udistrital.edu.co>
2024-11-18 16:49:21 -0500

    Portafolio inicial

---
+++ Frontend/src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#150016",
+        secondary: "#29104A",
+        accent1: "#522C5D",
+        accent2: "#845162",
+        soft: "#E3B6B1",
+        light: "#FFE3D8",
+      },
+    },
   },
   plugins: [],
-}
+};
