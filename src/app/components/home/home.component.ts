ca5659488946a16c24a7b0135d77db698917f037
Juan Manuel Serrano Rodriguez <jumserranor@udistrital.edu.co>
2024-11-26 08:54:36 -0500

    Interactuable svg

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -48,7 +48,7 @@ export class HomeComponent implements OnInit {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
 
-    const maxDistance = 0.8; // Máxima distancia que los ojos pueden moverse
+    const maxDistance = 0.8; 
 
     const angle = Math.atan2(mouseY - centerY, mouseX - centerX);
     const offsetX = Math.cos(angle) * maxDistance;
@@ -61,5 +61,78 @@ export class HomeComponent implements OnInit {
       this.renderer.setAttribute(eye1, 'transform', `translate(${offsetX*2.2}, ${offsetY*1.6})`);
       this.renderer.setAttribute(eye2, 'transform', `translate(${offsetX*2.5}, ${offsetY*2.3})`);
     }
+
+    // Animation hair
+    if (mouseY < rect.top + rect.height / 3 && mouseY > rect.top && mouseX > rect.left && mouseX < rect.right) {
+      const hairPaths = Array.from(svg.querySelectorAll('[inkscape\\:label="brillos"] path')).filter(path => path.id !== 'path862');
+      hairPaths.forEach(path => {
+      const randomX = (Math.random() - 0.1) * -1; 
+      const randomY = (Math.random() - 0.1) * 1; 
+      const randomRotate = (Math.random() - 0.5) * 0.5; 
+
+      this.renderer.setAttribute(path, 'transform', `translate(${randomX}, ${randomY}) rotate(${randomRotate})`);
+      });
+    }
+
+
+    // Animation t-shirt
+
+    if (mouseY > rect.top + (2.4 * rect.height) / 3 && mouseY < rect.bottom && mouseX > rect.left && mouseX < rect.right) {
+      const hairPaths = Array.from(svg.querySelectorAll('[inkscape\\:label="sombras"] path')).filter(path => {
+      const idNumber = parseInt(path.id.replace('path', ''), 10);
+      return idNumber >= 394 && idNumber <= 411;
+      });
+      hairPaths.forEach(path => {
+      const randomX = (Math.random() - 0.1) * -0.4; 
+      const randomY = (Math.random() - 0.1) * 0.4; 
+      const randomRotate = (Math.random() - 0.5) * 0.4; 
+
+      this.renderer.setAttribute(path, 'transform', `translate(${randomX}, ${randomY}) rotate(${randomRotate})`);
+      });
+    }
+
+    // Animation glases
+    if (mouseY > rect.top + rect.height / 3 && mouseY < rect.top + (1.6 * rect.height) / 3 && mouseX > (1.03*rect.left) && mouseX < (0.92*rect.right)) {
+      const sketchPaths = Array.from(svg.querySelectorAll('[inkscape\\:label="boceto"] path')).filter(path => {
+      const idNumber = parseInt(path.id.replace('path', ''), 10);
+      return idNumber >= 78 && idNumber <= 92;
+      });
+      sketchPaths.forEach(path => {
+      const randomX = (Math.random() - 0.1) * -0.3; 
+      const randomY = (Math.random() - 0.1) * 0.3;
+      const randomRotate = (Math.random() - 0.5) * 0.2; 
+
+      this.renderer.setAttribute(path, 'transform', `translate(${randomX}, ${randomY}) rotate(${randomRotate})`);
+      });
+    }
+
+    //Animation mouth
+    if (mouseY > rect.top + (1.6 * rect.height) / 3 && mouseY < rect.top + (2.3 * rect.height) / 3 && mouseX > (1.1*rect.left) && mouseX < (0.9*rect.right)) {
+      const sketchPaths = Array.from(svg.querySelectorAll('[inkscape\\:label="boceto"] path')).filter(path => {
+      const idNumber = parseInt(path.id.replace('path', ''), 10);
+      return [71, 73, 74, 75, 76, 77, 114, 115, 130, 180, 181].includes(idNumber);
+      });
+      sketchPaths.forEach(path => {
+      const randomX = (Math.random() - 0.1) * -0.2; 
+      const randomY = (Math.random() - 0.1) * 0.2; 
+      const randomRotate = (Math.random() - 0.5) * 0.2; 
+
+      this.renderer.setAttribute(path, 'transform', `translate(${randomX}, ${randomY}) rotate(${randomRotate})`);
+      });
+
+      const shadowPaths = Array.from(svg.querySelectorAll('[inkscape\\:label="sombras"] path')).filter(path => {
+      const idNumber = parseInt(path.id.replace('path', ''), 10);
+      return (idNumber >= 364 && idNumber <= 370) || (idNumber >= 385 && idNumber <= 387) || (idNumber >= 491 && idNumber <= 565);
+      });
+      shadowPaths.forEach(path => {
+      const randomX = (Math.random() - 0.1) * -0.2; 
+      const randomY = (Math.random() - 0.1) * 0.2; 
+      const randomRotate = (Math.random() - 0.5) * 0.2; 
+
+      this.renderer.setAttribute(path, 'transform', `translate(${randomX}, ${randomY}) rotate(${randomRotate})`);
+      });
+    }
+
   }
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        'comic': ['"Comic Sans MS"', 'sans-serif'],
+      },
       colors: {
         primary: "#150016",
         secondary: "#29104A",
