b96e22778e92dc9dfbb6ec779e04047cd28377c0
Juan Manuel Serrano Rodriguez <jumserranor@udistrital.edu.co>
2024-11-26 17:41:21 -0500

    Tools home

--- src/app/components/lenguages/lenguages.component.ts
+++ src/app/components/lenguages/lenguages.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lenguages',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lenguages.component.html',
   styleUrl: './lenguages.component.css'
 })
